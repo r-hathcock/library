876fa19ceba5b23400a9a78b3db7c030ff4decba
rhath32 <45647039+rhath32@users.noreply.github.com>
2020-01-15 11:56:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// array to store books
+let myLibrary = [];
+
+// constructor for book objects
+function Book(title, author, year, length) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.length = length;
+}
+
+// adds book to myLibrary
+function addBookToLibrary() {
+    
+}
+
+// updates page with new or deleted books
+function render() {
+
+}
---
+++ todo.md
@@ -0,0 +1,5 @@
+[ ] render() function that loops through array and displays each book on page. 
+[ ] NEW BOOK button
+[ ] REMOVE BOOK button
+[ ] READ STATUS buton
+[ ] OPTIONAL: localStorage
