0ba8bf71343edc135b31747d46e74b1f44d6f545
Ryan Hathcock <rhath32@gmail.com>
2020-01-18 10:50:23 -0800

    Added Modal box and listeners. Box is intended to retrieve book details from user

--- app.js
+++ app.js
@@ -9,9 +9,40 @@ function Book(title, author, year, length) {
     this.length = length;
 }
 
+var textNode = "hello";
 // adds book to myLibrary
+function displayModal() {
+    // popup modal to retrieve book information
+    let modal = document.querySelector(".modal");
+    let closeBtn = document.querySelector(".close-btn");
+    let addBookBtn = document.getElementById("addBook");
+
+    modal.style.display = "block";
+
+    addBookBtn.onclick = function() {
+        addBookToLibrary();
+        modal.style.display = "none";
+    } 
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(e) {
+        if (e.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
+}
+
 function addBookToLibrary() {
-    
+    var mainDiv = document.getElementById("main-container");
+    var newBook = document.createElement("div");
+    newBook.className = 'book';
+
+    var newContent = document.createTextNode(textNode);
+    newBook.appendChild(newContent);
+    mainDiv.appendChild(newBook);
 }
 
 // updates page with new or deleted books
